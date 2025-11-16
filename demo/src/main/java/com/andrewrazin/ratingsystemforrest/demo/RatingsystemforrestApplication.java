674e3b761e93039be1c4801cf951345e74474ea4
Sharosh322 <132645197+Sharosh322@users.noreply.github.com>
2025-11-16 19:17:06 +0400

    Done

---
+++ demo/src/main/java/com/andrewrazin/ratingsystemforrest/demo/RatingsystemforrestApplication.java
@@ -0,0 +1,13 @@
+package com.andrewrazin.ratingsystemforrest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsystemforrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsystemforrestApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/andrewrazin/ratingsystemforrest/demo/RatingsystemforrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewrazin.ratingsystemforrest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsystemforrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
